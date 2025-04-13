0d1bb40991f1dc2c414bb28ce22ee3d5e4908d46
dcrauwels <d.crauwels@gmail.com>
2025-04-13 21:12:26 +0200

    token validation done. write unit tests next

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -19,5 +20,45 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 }
 
 func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
-	pass
+	// define claims to unpack into and keyfunc
+	claims := &jwt.RegisteredClaims{}
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		//check if signed with HMAC
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(tokenSecret), nil
+	}
+
+	// parse the token
+	token, err := jwt.ParseWithClaims(tokenString, claims, keyFunc)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	// token checks
+	// check if token is valid
+	if !token.Valid {
+		return uuid.Nil, fmt.Errorf("invalid token")
+	}
+	// check if token is expired
+	if claims.ExpiresAt.Time.Before(time.Now()) {
+		return uuid.Nil, fmt.Errorf("token expired")
+	}
+	// check if token is issued in the future
+	if claims.IssuedAt != nil && claims.IssuedAt.Time.After(time.Now()) {
+		return uuid.Nil, fmt.Errorf("token issued in the future")
+	}
+	// check if token is issued by the correct issuer
+	if claims.Issuer != "chirpy" {
+		return uuid.Nil, fmt.Errorf("token issued by incorrect issuer")
+	}
+
+	// get userID from token
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid userID in token: %v", err)
+	}
+	// return userID
+	return userID, nil
 }
